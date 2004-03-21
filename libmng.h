bff66f85da2f4d1add5cc29fde7983852be078fe
gjuyn <gjuyn>
2004-03-21 17:52:41 +0000

    Cleanup for new 1.0.7 release

--- libmng.h
+++ libmng.h
@@ -102,7 +102,7 @@
 /* ************************************************************************** */
 /* *                                                                        * */
 /* * project   : libmng                                                     * */
-/* * file      : libmng.h                  copyright (c) 2000-2003 G.Juyn   * */
+/* * file      : libmng.h                  copyright (c) 2000-2004 G.Juyn   * */
 /* * version   : 1.0.7                                                      * */
 /* *                                                                        * */
 /* * purpose   : main application interface                                 * */
@@ -411,13 +411,13 @@ extern "C" {
 /* *                                                                        * */
 /* ************************************************************************** */
 
-#define MNG_VERSION_TEXT    "1.0.7-alpha"
+#define MNG_VERSION_TEXT    "1.0.7"
 #define MNG_VERSION_SO      1          /* eg. libmng.so.1  */
 #define MNG_VERSION_DLL     1          /* but: libmng.dll (!) */
 #define MNG_VERSION_MAJOR   1
 #define MNG_VERSION_MINOR   0
 #define MNG_VERSION_RELEASE 7
-#define MNG_VERSION_BETA    MNG_TRUE
+#define MNG_VERSION_BETA    MNG_FALSE
 
 MNG_EXT mng_pchar MNG_DECL mng_version_text      (void);
 MNG_EXT mng_uint8 MNG_DECL mng_version_so        (void);
